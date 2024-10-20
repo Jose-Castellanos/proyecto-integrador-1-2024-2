232ab1f5cb58275629f0166cda7c22dca544956a
Jose Castellanos <jmcastellanos153@misena.edu.co>
2024-10-19 23:19:48 -0500

    evento con la letra s

--- sesion 10/sesion-10-pi-2024-2-middle/src/pages/home/Home.jsx
+++ sesion 10/sesion-10-pi-2024-2-middle/src/pages/home/Home.jsx
@@ -6,7 +6,7 @@ import Beach from "./world/Beach";
 import Staging from "./staging/Staging";
 import { Loader, PositionalAudio } from "@react-three/drei";
 import { Perf } from "r3f-perf";
-import { Suspense, useCallback, useRef } from "react";
+import { Suspense, useCallback, useRef,useEffect } from "react";
 import Video from "./world/Video";
 
 const Home = () => {
@@ -17,13 +17,27 @@ const Home = () => {
   const audioRef = useRef(null);
   
   const handleSound = useCallback(() => {
-    audioRef.current.play();
-    audioRef.current.setVolume(10);
+    if (audioRef.current) {
+      audioRef.current.play();
+      audioRef.current.setVolume(10); 
+    }
   }, []);
+  useEffect(() => {
+    const handleKeyDown = (event) => {
+      if (event.key.toLowerCase() === 's') {
+        handleSound(); 
+      }
+    };
+    window.addEventListener('keydown', handleKeyDown);
+
+    return () => {
+      window.removeEventListener('keydown', handleKeyDown);
+    };
+  }, [handleSound]);
 
   return (
     <>
-      <Canvas camera={cameraSettings} onClick={handleSound}>
+      <Canvas camera={cameraSettings} >
         <Suspense fallback={null}>
           <Perf position={"top-left"} />
           <Controls />
